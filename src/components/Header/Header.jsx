cb60024097d6918e96ba681c5c5864c1995e4d07
Караваев Никита <karavaevna@it2g.ru>
2020-05-13 23:41:00 +0500

    Разбивка на компоненты

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="App">
+      <a href="#a">Home</a>
+      <a href="#b">Messages</a>
+      <a href="#c">News</a>
+    </div>
+  );
+};
---
+++ src/components/Technologies/Technologies.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Technologies = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>jest</li>
+      </ul>
+    </div>
+  );
+};
