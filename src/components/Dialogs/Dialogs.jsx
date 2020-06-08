6941f3c3c2e3c14af091271c792ef020f83918f9
Караваев Никита <karavaevna@it2g.ru>
2020-06-08 16:04:45 +0500

    Страница сообщений

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,35 +4,41 @@ import { Messages } from "./Messages";
 
 import s from "./Dialogs.module.scss";
 
-export const Dialogs = () => {
-  const interlocutor = [
-    { id: 1, name: "Nikita" },
-    { id: 2, name: "Nikitos" },
-    { id: 3, name: "Asos" },
-    { id: 4, name: "Nik" },
-    { id: 5, name: "Nikitochka" },
-  ];
+const interlocutor = [
+  { id: 1, name: "Nikita" },
+  { id: 2, name: "Nikitos" },
+  { id: 3, name: "Asos" },
+  { id: 4, name: "Nik" },
+  { id: 5, name: "Nikitochka" },
+];
 
-  const messages = [
-    { id: 1, name: "Hi! how are you?" },
-    { id: 2, name: "what`s up? are you busy?" },
-    { id: 3, name: "i don`t belive you!" },
-  ];
+const messages = [
+  { id: 1, text: "Hi! how are you?" },
+  { id: 2, text: "what`s up? are you busy?" },
+  { id: 3, text: "i don`t belive you!" },
+];
+
+export const Dialogs = () => {
+  const inter =
+    interlocutor &&
+    interlocutor.map(({ id, name }) => (
+      <div key={id}>
+        <Interlocutor name={name} />
+      </div>
+    ));
+
+  const messagesFromInter =
+    messages &&
+    messages.map(({ id, text }) => (
+      <div key={id}>
+        <Messages text={text} />
+      </div>
+    ));
 
   return (
     <div className={s.dialogs}>
-      <div className={s.dialogs__names}>
-        <Interlocutor name="Nikita" id="1" />
-        <Interlocutor name="Nikitos" id="2" />
-        <Interlocutor name="Asos" id="3" />
-        <Interlocutor name="Nik" id="4" />
-        <Interlocutor name="Nikitochka" id="5" />
-      </div>
-      <div className={s.dialogs__messages}>
-        <Messages text="Hi! how are you?" />
-        <Messages text="what`s up? are you busy?" />
-        <Messages text="i don`t belive you!" />
-      </div>
+      <div className={s.dialogs__names}>{inter}</div>
+      <div className={s.dialogs__messages}>{messagesFromInter}</div>
     </div>
   );
 };
--- src/components/Dialogs/Interlocutor/Interlocutor.jsx
+++ src/components/Dialogs/Interlocutor/Interlocutor.jsx
@@ -5,9 +5,10 @@ export const Interlocutor = (props) => {
   const { name, id } = props;
 
   const path = "/dialogs/" + id;
+
   return (
     <div>
-      <NavLink to={path}>{name}</NavLink>
+      <NavLink to={`dialogs/${id}`}>{name}</NavLink>
     </div>
   );
 };
